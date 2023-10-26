0414f47e95965a6e597b71155b4eb27ce68d5e32
ichbinkaufmann <adri.firdiansyah17038@student.unsika.ac.id>
2023-10-27 05:34:50 +0700

    first commit

---
+++ src/components/SkeletonLoader.vue
@@ -0,0 +1,9 @@
+<script setup></script>
+
+<template>
+  <h1>This is skeleton page</h1>
+</template>
+
+<style>
+@import url("../assets/style/page.css");
+</style>
---
+++ src/components/StoreCatalog.vue
@@ -0,0 +1,11 @@
+<script setup>
+import SkeletonLoader from "./SkeletonLoader.vue";
+</script>
+
+<template>
+  <h1>hello world</h1>
+</template>
+
+<style>
+@import url("../assets/style/page.css");
+</style>
